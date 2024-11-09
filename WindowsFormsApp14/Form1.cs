d3fc5fdfb163e53ec515f05f75f5c18ad3c88774
ArmanGoodarzi <148266697+ArmanGoodarzi@users.noreply.github.com>
2024-11-09 15:53:17 +0330

    Practice3
    The fourth session

---
+++ WindowsFormsApp14/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp14
+{
+    public partial class Form1 : Form
+    {
+        private Car car;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string brand = textBox1.Text;
+                int speed = int.Parse(textBox2.Text);
+
+                car = new Car(brand,speed);
+
+                label1.Text = "Car Details: " + car.GetDetails();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter a valid number for speed or brand.");
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class Car
+    {
+        private string brand;
+        private int speed;
+
+        public Car(string b,int s)
+        {
+            brand = b;
+            speed = s;
+            MessageBox.Show($"Car created: Brand = {brand} , Speed = {speed}");
+        }
+
+        ~Car()
+        {
+            MessageBox.Show($"Car destroyed: Brand =  { brand} , Speed = {speed}" );
+        }
+
+        public string GetDetails()
+        {
+            return $"Brand: {brand}, Speed: {speed}";
+        }
+    }
+}
